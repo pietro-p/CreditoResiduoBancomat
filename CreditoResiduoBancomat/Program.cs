980716a14b1bb7b08ec3d235214c19f3c85c234c
_ Trentini _ <pietrotrentini5@gmail.com>
2020-10-15 09:44:44 +0200

    scrittura codice CreditoResiduoBancomat

--- CreditoResiduoBancomat/Program.cs
+++ CreditoResiduoBancomat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CreditoResiduoBancomat
 {
@@ -6,7 +7,33 @@ namespace CreditoResiduoBancomat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double saldo, spese, importospesa, totspesa = 0;
+
+            Console.Write("inserisci il saldo del tuo portafoglio:");
+            saldo = double.Parse(Console.ReadLine());
+            Console.Write("inserisci il numero di spese che vuoi fare:");
+            spese = double.Parse(Console.ReadLine());
+            while (totspesa < spese)
+            {
+                Console.Write("inserisci l'importo della spesa: ");
+                importospesa = double.Parse(Console.ReadLine());
+                if (importospesa > saldo)
+                {
+                    Console.WriteLine("saldo non sufficiente");
+                }
+                else
+                {
+                    saldo = saldo - importospesa;
+                    Console.WriteLine($"dopo la tua spesa ti rimarranno:{saldo}");
+                }
+                totspesa++;
+            }
+            Console.WriteLine($"Hai eseguito {totspesa} acquisti, saldo rimanente:{saldo}");
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
